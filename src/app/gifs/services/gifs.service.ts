111ee60a3aad1e2073ca269bd4725317e99d91bd
Andrea Cedeno <andreaccdreis@gmail.com>
2023-09-22 16:47:05 -0400

    todo

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,68 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SearchReponse, Gif } from '../interfaces/gifs.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  public gifList: Gif[] = [];
+
+  private _tagsHistory: string[]=[];
+  private apiKey: string = 'Vo3yNIsEUOVNVVAJts6g1Rlj2ArfQE7k'
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
+
+  constructor( private http: HttpClient) { this.loadLocalStorage(); }
+
+  get tagsHistory() { 
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+
+  }
+
+
+  private saveLocalStorage() {
+    localStorage.setItem('history', JSON.stringify( this._tagsHistory ));
+  }
+
+  private loadLocalStorage() {
+    if(localStorage.getItem('history')){
+      const temporal = localStorage.getItem('history');
+      this._tagsHistory = JSON.parse(temporal!);
+      
+      if(this._tagsHistory.length === 0) return;
+      this.searchTag(this._tagsHistory[0]);
+    }else{
+      return;
+    }
+  }
+
+  searchTag( tag: string): void{
+    if (tag.length === 0 ) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit','10')
+    .set('q', tag)
+
+    this.http.get<SearchReponse>(`${ this.serviceUrl }/search?`, { params})
+    .subscribe(resp => {
+        this.gifList = resp.data;
+        
+    });
+
+  }
+
+}
